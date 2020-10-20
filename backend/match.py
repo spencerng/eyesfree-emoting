c575a9f2d5be98a953f83ac6fa8c82f0b01afa99
Spencer Ng <sng1488@gmail.com>
2020-10-20 01:47:59 -0500

    Add basic contour matching

---
+++ backend/match.py
@@ -0,0 +1,35 @@
+import cv2
+import sys
+
+emotes = ["angry", "joy", "laugh", "like", "love", "sad"]
+
+
+def match(img):
+    img_gray = cv2.imread(img, 0)
+    ret, thresh = cv2.threshold(img_gray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, 2, 1)[-2:]
+    cnt1 = contours[0]
+
+    similarity = dict()
+    for emote in emotes:
+        template = cv2.imread(f"templates/{emote}.png", 0)
+        # TODO: Fix this scaling
+        template = cv2.resize(
+            template, (int(template.shape[1] * 0.6), int(template.shape[0] * 0.6))
+        )
+
+        ret, thresh2 = cv2.threshold(template, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thresh2, 2, 1)[-2:]
+        cnt2 = contours[0]
+
+        # This generates a similarity score
+        ret = cv2.matchShapes(cnt1, cnt2, 1, 0.0)
+
+        similarity[emote] = ret
+        
+    similarity = dict(sorted(similarity.items(), key=lambda x: x[1], reverse=True))
+    print(similarity)
+
+
+if __name__ == "__main__":
+    match(sys.argv[1])
--- backend/testing.py
+++ backend/testing.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('pic1.png')
+img = cv2.imread("pic1.png")
 imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 cv2.imshow("img", img)
 for contour in contours:
-    approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+    approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
     cv2.drawContours(img, [approx], 0, (0, 0, 0), 5)
     x = approx.ravel()[0]
     y = approx.ravel()[1] - 5
@@ -23,7 +23,7 @@ for contour in contours:
     elif len(approx) == 10:
         cv2.putText(img, "Star", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
     else:
-        print (len(approx))
+        print(len(approx))
         print("hello")
         cv2.putText(img, "Circle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
