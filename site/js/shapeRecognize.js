93fcf4ca6f2516b360d7924165ab27d109f8c339
Spencer Ng <sng1488@gmail.com>
2020-10-20 23:51:51 -0500

    Add shape detection prototype

--- site/js/shapeRecognize.js
+++ site/js/shapeRecognize.js
@@ -1,146 +1,148 @@
-$(document).ready(function(){
-  window.Canvas.init()
 
-  var recognizer = new DollarRecognizer();
+//
+// Startup
+//
+var _isDown, _points, _strokeID, _r, _g, _rc; // global variables
+function onLoadEvent()
+{
+  _points = new Array(); // point array for current stroke
+  _strokeID = 0;
+  _r = new DollarRecognizer();
 
-  var strokes = [];
-  var stroking = []
+  var canvas = document.getElementById('myCanvas');
+  _g = canvas.getContext('2d');
+  _g.lineWidth = 3;
+  _g.font = "16px Gentilis";
+  _rc = getCanvasRect(canvas); // canvas rect on page
+  
 
-  var render = function(){
-    window.Canvas.clear()
-    console.log("Rendering")
-    _.each(strokes, function(strk){
-      window.Canvas.path(strk, 3, "#777777");
-    })
+  _isDown = false;
+}
+function getCanvasRect(canvas)
+{
+  var w = canvas.width;
+  var h = canvas.height;
 
-    window.Canvas.path(stroking, 3, "#000000");
-
-    if( !drawing && strokes.length){
-      var _strokes = strokes.map(function(stroke){
-                                      return stroke.map(function(p){
-                                        return new nDollar.Point(p.x, p.y);
-                                      })
-                                });
-
-      var useBoundedRotationInvariance = $("input[name=boundRotation]").is(':checked');
-      var requireSameNoOfStrokes = $("input[name=sameStrokeCount]").is(':checked');
-      var useProtractor = $("input[name=alghorithm]:checked").val() == 'protactor';
-
-      var result = recognizer.Recognize( _strokes, useBoundedRotationInvariance, requireSameNoOfStrokes, useProtractor);
-      console.log(result);
-      var text = "Gesture result: " + result.Name + ", score: "+ result.Score+" points\n<H1>";
-      $("#output").html(text);
+  var cx = canvas.offsetLeft;
+  var cy = canvas.offsetTop;
+  while (canvas.offsetParent != null)
+  {
+    canvas = canvas.offsetParent;
+    cx += canvas.offsetLeft;
+    cy += canvas.offsetTop;
+  }
+  return {x: cx, y: cy, width: w, height: h};
+}
+function getScrollX()
+{
+  var scrollX = $(window).scrollLeft();
+  return scrollX;
+}
+function getScrollY()
+{
+  var scrollY = $(window).scrollTop();
+  return scrollY;
+}
+//
+// Mouse Events
+//
+function mouseDownEvent(x, y, button)
+{
+  document.onselectstart = function() { return false; } // disable drag-select
+  document.onmousedown = function() { return false; } // disable drag-select
+  if (button <= 1)
+  {
+    _isDown = true;
+    x -= _rc.x - getScrollX();
+    y -= _rc.y - getScrollY();
+    if (_strokeID == 0) // starting a new gesture
+    {
+      _points.length = 0;
+      _g.clearRect(0, 0, _rc.width, _rc.height);
     }
-
-
+    _points[_points.length] = new Point(x, y, ++_strokeID);
+    var clr = "rgb(" + rand(0,200) + "," + rand(0,200) + "," + rand(0,200) + ")";
+    _g.strokeStyle = clr;
+    _g.fillStyle = clr;
+    _g.fillRect(x - 4, y - 3, 9, 9);
   }
 
-  var drawing = false;
-
-
-
-
-  $("canvas:first")
-  .mousedown(function(e){
-      drawing = true;
-      stroking.push({x:e.offsetX, y:e.offsetY})
-      render();
-  })
-  .mousemove(function(e){
-     if(drawing){
-      stroking.push({x:e.offsetX, y:e.offsetY})
-      render();
+}
+function mouseMoveEvent(x, y, button)
+{
+  if (_isDown)
+  {
+    x -= _rc.x - getScrollX();
+    y -= _rc.y - getScrollY();
+    _points[_points.length] = new Point(x, y, _strokeID); // append
+    drawConnectedPoint(_points.length - 2, _points.length - 1);
+  }
+}
+function printPoints() {
+  var pointStr = "new Array("
+  for (var i = 0; i < _points.length; i++) {
+    pointStr += "new Point(" + _points[i].X
+    pointStr += "," + _points[i].Y
+    pointStr += "),"
+  }
+  pointStr = pointStr.slice(0, -1) + ")"
+  console.log(pointStr)
+}
+function mouseUpEvent(x, y, button)
+{
+  printPoints()
+  document.onselectstart = function() { return true; } // enable drag-select
+  document.onmousedown = function() { return true; } // enable drag-select
+  if (button <= 1)
+  {
+    if (_isDown)
+    {
+      _isDown = false;
     }
-  })
-  .mouseup(function(e){
-    if(drawing){
-      drawing = false;
-      stroking.push({x:e.offsetX, y:e.offsetY})
-      strokes.push(stroking);
-      stroking = [];
-      render();
+  }
+}
+function detectEmoticon() {
+  var statusText = document.getElementsByClassName("status")[0]
+  if (_points.length >= 10)
+    {
+      var result = _r.Recognize(_points);
+      statusText.innerHTML = result.Name + " detected (" + round(result.Score,2) + ")";
     }
-  });
-
-});
-
-
-window.Canvas = new (function(){
-
-
-  var canvas;
-  this.init = function(){
-    canvas = document.getElementsByTagName("canvas")[0];
-    this.context = canvas.getContext("2d");
-  }
-
-  this.clear = function(){
-      this.context.clearRect(0, 0, canvas.width, canvas.height);
-  }
-
-  this.text = function(txt, p, color, offset=0){
-    this.context.fillStyle = color || '#000000';
-    this.context.fillText(txt,p.x+offset,p.y+offset);
-  }
-
-  this.point = function(p, r, color, name, offset){
-    offset = offset || {x:0, y:0};
-    this.context.fillStyle = color || '#000000';
-    this.context.beginPath();
-    this.context.arc(p.x + offset.x, p.y + offset.y, r || 3, 0, 2 * Math.PI, false);
-    this.context.fill();
-    name && name.length && this.text(name, p, color, r + offset.y);
-  }
-
-  this.circle = function(p, r, color, name){
-    this.context.strokeStyle = color || '#000000';
-    this.context.lineWidth = 1;
-    this.context.beginPath();
-    this.context.arc(p.x, p.y, r || 3, 0, 2 * Math.PI, false);
-    this.context.stroke();
-    name && name.length && this.text(name, p, color, r);
-  }
-
-  this.rect = function(bound, w, color, name){
-    this.context.strokeStyle = color || '#000000';
-    this.context.lineWidth = w;
-    this.context.rect(bound.left, bound.top, bound.right-bound.left, bound.bottom-bound.top);
-    this.context.stroke();
-    name && name.length && this.text(name, {x:bound.right, y:bound.bottom}, color, w+1);
-  }
-
-  this.line = function(p1, p2, thickness, color, name, offset){
-    offset = offset || {x:0, y:0};
-    this.context.strokeStyle = color || '#000000';
-    this.context.lineWidth = thickness;
-    this.context.beginPath();
-    this.context.moveTo(p1.x + offset.x, p1.y + offset.y);
-    this.context.lineTo(p2.x + offset.x, p2.y + offset.y);
-    this.context.stroke();
-    name && name.length && this.text(name, {x:(p1.x+p2.x+ offset.x)/2 + 5, y:(p1.y+p2.y+ offset.x)/2 + 10}, color);
-  }
-
-  var getColor = function(c, i){
-    if(typeof c=='object' && length in c)
-      return c[i%c.length]
-
-    return c;
-  }
-
-  this.path = function(points, thickness, color, name){
-    var prev = points[0];
-    //console.log(points)
-    for(var i=1;i<points.length;i++){
-      var p = points[i];
-      this.context.strokeStyle = getColor(color, i);
-      this.context.lineWidth = thickness;
-      this.context.beginPath();
-      this.context.moveTo(prev.x, prev.y);
-      this.context.lineTo(p.x, p.y);
-      this.context.stroke();
-      prev = p;
+    else
+    {
+      statusText.innerHTML = "no emoticon detected"
     }
-    name && name.length && this.text(name, points[points.length-1], getColor(color, 0));
-  }
+    _strokeID = 0; // signal to begin new gesture on next mouse-down
+}
 
-})();
+function drawConnectedPoint(from, to)
+{
+  _g.beginPath();
+  _g.moveTo(_points[from].X, _points[from].Y);
+  _g.lineTo(_points[to].X, _points[to].Y);
+  _g.closePath();
+  _g.stroke();
+}
+function drawText(str)
+{
+  _g.fillStyle = "rgb(255,255,136)";
+  _g.fillRect(0, 0, _rc.width, 20);
+  _g.fillStyle = "rgb(0,0,255)";
+  _g.fillText(str, 1, 14);
+}
+function rand(low, high)
+{
+  return Math.floor((high - low + 1) * Math.random()) + low;
+}
+function round(n, d) // round 'n' to 'd' decimals
+{
+  d = Math.pow(10, d);
+  return Math.round(n * d) / d;
+}
+
+function onClickClearStrokes()
+{
+  _points.length = 0;
+  _strokeID = 0;
+  _g.clearRect(0, 0, _rc.width, _rc.height);
+}
