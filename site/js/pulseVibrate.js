930dd9d6b96da32364cc3a42f2c4f13afa9a504f
Spencer Ng <sng1488@gmail.com>
2020-10-20 19:11:06 -0500

    Kill js tuples

--- site/js/pulseVibrate.js
+++ site/js/pulseVibrate.js
@@ -7,18 +7,19 @@ function sendPulseSequence(duration, count) {
 		// This is the pause duration
 		pattern.push(duration * 1.25)
 	}
+	console.log(pattern)
 	navigator.vibrate(pattern)
 }
 
 document.onclick = function sendEmojiPulse(e) {
 	var type = e.target.src
 	let mapping = {
-		"love": (100, 1),
-		"yay": (100, 2),
-		"laugh": (100, 3),
-		"like": (300, 1),
-		"sad": (300, 2),
-		"angry": (300, 3)
+		"love": [100, 1],
+		"yay": [100, 2],
+		"laugh": [100, 3],
+		"like": [300, 1],
+		"sad": [300, 2],
+		"angry": [300, 3]
 	};
 	var statusText = document.getElementsByClassName("status")[0]
 	
