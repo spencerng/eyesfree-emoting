c40480795963c42a1285be517101a0cff78f71cd
huangdaweiUCHICAGO <huangdawei@uchicago.edu>
2020-10-22 18:26:21 -0400

    Add files via upload

--- scrollWheel(Dawei)/wheel.js
+++ scrollWheel(Dawei)/wheel.js
@@ -1,5 +1,18 @@
 var emoji = "None"
 
+function onLoadEvent()
+{
+  _points = new Array(); // point array for current stroke
+  _strokeID = 0;
+  _r = new DollarRecognizer();
+
+  var canvas = document.getElementById('myCanvas');
+  canvas.addEventListener("touchstart", touchStartEvent, false);
+  canvas.addEventListener("touchmove", touchMoveEvent, false);
+  canvas.addEventListener("touchend", touchEndEvent, false);
+
+}
+
 function ab1_Click() {
    var vid1 = document.getElementById("wheel");
    if (vid1.style.visibility == "visible") { v = "hidden"; }
@@ -31,13 +44,28 @@ function myFunction() {
   }
 }
 
-function userClicked() {
+function mouseDown1() {
     var x = event.clientX;
     var y = event.clientY;
+	document.getElementById("x").innerHTML = x;
+	document.getElementById("y").innerHTML = y;
+	
+	var width = document.body.clientWidth;
+	var height = document.body.clientHeight;
     var wheelSel = document.getElementById("wheel");
     wheelSel.style.display = 'block';
-    wheelSel.style.position = 'center';
+    wheelSel.style.position = 'absolute';
+	wheelSel.style.left = x-width/2 + 100 + 'px'
+	wheelSel.style.top = y-200 + 'px'
 }
+
+function mouseUp1() {
+
+    var wheelSel = document.getElementById("wheel");
+    wheelSel.style.display = 'none';
+    wheelSel.style.position = 'absolute';
+}
+
 
 function myFunction1() {
   document.getElementById("demo").innerHTML = emoji;
@@ -45,4 +73,17 @@ function myFunction1() {
 
 function updateEmoji(em){
 	document.getElementById("update").innerHTML = em;
+}
+
+function touchStartEvent(event) {
+   mouseDownEvent(event.touches[0].pageX, event.touches[0].pageY, 0)
+}
+
+function touchMoveEvent(event) {
+  mouseMoveEvent(event.touches[0].pageX, event.touches[0].pageY, 0);
+  event.preventDefault();
+}
+
+function touchEndEvent(event) {
+  mouseUpEvent(event.changedTouches[0].pageX, event.changedTouches[0].pageY, 0);
 }
