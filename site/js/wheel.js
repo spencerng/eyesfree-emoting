706821e87bfe9e72fd891ecfe17d7445ac6b8013
Spencer Ng <sng1488@gmail.com>
2020-10-23 18:52:48 -0500

    Add disable selection code

--- site/js/wheel.js
+++ site/js/wheel.js
@@ -2,6 +2,20 @@ var lastTouched = undefined;
 var emojis = ["yay", "sad", "angry", "laugh", "love", "like"]
 var showWheel;
 
+$.fn.extend({
+    disableSelection: function() {
+        this.each(function() {
+            this.onselectstart = function() {
+                return false;
+            };
+            this.unselectable = "on";
+            $(this).css('-moz-user-select', 'none');
+            $(this).css('-webkit-user-select', 'none');
+        });
+        return this;
+    }
+});
+
 function onLoadEvent()
 {
   var canvas = document.getElementById('myCanvas');
