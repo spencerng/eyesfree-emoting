c0c637f97d704d801158bc2da779d5eeafc5e7fc
Spencer Ng <sng1488@gmail.com>
2020-10-24 19:16:39 -0400

    Update wheel.js

--- js/wheel.js
+++ js/wheel.js
@@ -25,11 +25,6 @@ function onLoadEvent()
   document.onmousedown = mouseDown
   document.onmouseup = mouseUp
 
-  $(document).ready(function(){
-
-     $('.notSelectable').disableSelection();
-
-  });
 
   canvas.addEventListener("touchstart", touchStartEvent, false);
   canvas.addEventListener("touchmove", touchMoveEvent, false);
